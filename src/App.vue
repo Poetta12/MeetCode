a71bd9e7563b4f577ac0644c533823bc00d0f6c5
Pedro Costa <163901231+Poettawds@users.noreply.github.com>
2024-07-29 02:49:33 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <img alt="Vue logo" id="logo" class="logo" src="/logo.png" width="125" height="125" />
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Pedro t'offre la MeetCard !" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
@@ -25,6 +19,7 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+  border-radius: 100%;
 }
 
 @media (min-width: 1024px) {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,94 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+import { ref } from 'vue';
+
+const instaUrl = ref('https://www.instagram.com/poettawds');
+const whatsappUrl = ref('https://wa.me/33753812381');
+const telegramUrl = ref('https://t.me/Poettawds');
+
+// Fonction pour gérer le clic sur le QR code
+function handleQrClick(url) {
+  window.open(url, '_blank');
+}
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Je ne voulais pas te déranger, mais tu me plaît. Ça serait cool de faire connaissance 😇. Je te laisse mon contact 😊.
     </h3>
+    <p>
+      Pour lui répondre:
+    </p>
+    <div id="insta">
+      <img src="/QR/poettawds_qr.png" alt="insta QR" @click="handleQrClick(instaUrl)">
+    </div>
+    <div id="whatsapp">
+      <img src="/QR/whatsapp_qr.png" alt="whatsapp QR" @click="handleQrClick(whatsappUrl)">
+    </div>
+    <div id="telegram">
+      <img src="/QR/telegram_qr.png" alt="telegram QR"  @click="handleQrClick(telegramUrl)">
+    </div>
   </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
+img {
+  max-width: 100%;
+  border-radius: 20px;
+  cursor: pointer; /* Ajoute un curseur pointer pour indiquer que l'image est cliquable */
+}
+
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
+  font-weight: 400;
+  font-size: 1.5rem;
   position: relative;
   top: -10px;
+  color: whitesmoke;
 }
 
 h3 {
-  font-size: 1.2rem;
+  font-size: 1rem;
+  color: #333;
+  background: whitesmoke;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 20px;
 }
 
-.greetings h1,
-.greetings h3 {
+.greetings {
+  font-family: 'Poppins', sans-serif;
+}
+
+.greetings h1 {
   text-align: center;
+}
+
+.greetings h3 {
+  text-align: justify;
+}
+
+.greetings div {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+}
+
+.greetings > p {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.greetings div p {
+  font-size: 2rem;
+}
+
+.greetings div img {
+  width: 50%;
 }
 
 @media (min-width: 1024px) {
