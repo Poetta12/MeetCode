01a29ab6828e999764046871ae08dc41c9a5fbb1
Pedro Costa <163901231+Poettawds@users.noreply.github.com>
2024-07-29 03:10:46 +0200

    dependencies update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
+iimport { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/src/main.js',
+    },
+  },
 })
